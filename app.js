f2e655bab452d9e92206000df4e5f565a735e0b9
shahjalalcse <shah-jalal1>
2021-02-14 00:49:58 +0600

    part 5 unselect image done

--- app.js
+++ app.js
@@ -46,14 +46,15 @@ const getImages = (query) => {
 let slideIndex = 0;
 const selectItem = (event, img) => {
   let element = event.target;
-  element.classList.add('added');
+  element.classList.toggle('added');
  
   let item = sliders.indexOf(img);
   if (item === -1) {
     sliders.push(img);
-  } else {
-    alert('Hey, Already added !')
-  }
+  } // else {
+  //   // alert('Hey, Already added !')
+  //   // element.classList.remove('added');
+  // }
 }
 var timer
 const createSlider = () => {
